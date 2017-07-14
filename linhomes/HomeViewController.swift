d50eace0f879458683105b045d40fc69ff3fee40
Leon <tplus.tcl@gmail.com>
2017-07-14 04:24:29 -0700

    add listener

--- linhomes/HomeViewController.swift
+++ linhomes/HomeViewController.swift
@@ -12,15 +12,36 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var uiSecurity: UIView!
     @IBOutlet weak var uiWeather: UIView!
-    @IBOutlet weak var uiScroll: UIScrollView!
+    @IBOutlet weak var uiMedia: UIView!
+    @IBOutlet weak var uiControl: UIView!
+    @IBOutlet weak var uiHealth: UIView!
+    @IBOutlet weak var uiTree: UIView!
+    @IBOutlet weak var uiSetting: UIView!
+    @IBOutlet weak var uiMore: UIView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        uiScroll.contentSize = CGSize(width: self.view.frame.size.width, height: 600)
-//        uiScroll.setContentOffset(CGPoint(x: 0, y: max(uiScroll.contentSize.height - uiScroll.bounds.size.height, 0) ), animated: true)
+        let tapSecurity = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        let tapWeather = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        let tapMedia = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        let tapControl = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        let tapHealth = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        let tapTree = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        let tapSetting = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        let tapMore = UITapGestureRecognizer(target: self, action: #selector(handleTapServices))
+        uiSecurity.addGestureRecognizer(tapSecurity)
+        uiWeather.addGestureRecognizer(tapWeather)
+        uiMedia.addGestureRecognizer(tapMedia)
+        uiControl.addGestureRecognizer(tapControl)
+        uiHealth.addGestureRecognizer(tapHealth)
+        uiTree.addGestureRecognizer(tapTree)
+        uiSetting.addGestureRecognizer(tapSetting)
+        uiMore.addGestureRecognizer(tapMore)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,15 +49,24 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.contentSize = CGSize(width: 1.0, height: stackView.frame.size.height)
+        scrollView.delaysContentTouches = false
     }
-    */
-
+    
+    func handleTapServices(_ sender: UITapGestureRecognizer) {
+        print("handleTapServices");
+        UIView.animate(withDuration: 0.2, delay: 0.0, options:[UIViewAnimationOptions.curveLinear], animations: {
+            sender.view?.backgroundColor = UIColor(red: 128.0/255.0, green: 128.0/255.0, blue: 128.0/255.0, alpha: 1.0)
+            sender.view?.backgroundColor = UIColor(red: 0.167707, green: 0.157497, blue: 0.167455, alpha: 1.0)
+        }, completion: nil)
+        
+//        if sender.view == uiSecurity{
+//            print("uiSecurity");
+//        } else if sender.view == uiWeather{
+//            print("uiWeather");
+//        }
+    }
+    
 }
